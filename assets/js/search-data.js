875e66f87356a598dc66c04344b718656bbaa5c7
renatocastro33 <38185291+renatocastro33@users.noreply.github.com>
2025-11-29 22:58:39 +0000

    Deploying to gh-pages from @ renatocastro33/renatocastro33.github.io@ba5071098c952c53f48fd1034da3892cab1ef5a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -499,14 +499,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/renatocastro.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%63%61%73%74%72%6F%63@%70%75%63%70.%65%64%75.%70%65", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -527,14 +527,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=0iUfJJEAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/renato-castro-cruz-317404137/", "_blank");
         },
       },{
       id: 'light-theme',
